fcc1f2d662412379e592a731e4ddfe95e6baec54
Nicole Steadman <nsteadman@gmail.com>
2022-03-07 12:31:21 -0800

    commit tree ca2a2dcd3dbf4fad5cd038a8e491393c60a98bea

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    triangle_valid
+    if x == y && y == z
+      :equilateral
+    elsif x == y || x == z || y == z
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def greater_than_zero
+    [x, y, z].all?(&:positive?)
+  end
+  
+  def triangel_inequality
+    x + y > z && y + z > x && z + x > y
+  end
+
+  def triangle_valid
+    raise TriangleError unless greater_than_zero && triangel_inequality
+  end
+
+  class TriangleError < StandardError
+  end  
 end
